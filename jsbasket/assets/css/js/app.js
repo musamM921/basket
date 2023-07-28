c931451439ca7921a25e4eff6bca0a7f6bd44700
musamM921 <135432572+musamM921@users.noreply.github.com>
2023-07-28 23:18:11 +0400

    update14

---
+++ jsbasket/assets/css/js/app.js
@@ -0,0 +1,53 @@
+let openShopping = document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+let products = [
+    {
+      id: 1,
+      name: 'PRODUCT NAME 1',
+      Image: '1.PNG',
+      price: 120000
+    },
+    {
+        id: 2,
+        name: 'PRODUCT NAME 2',
+        image: '2.PNG',
+        price: 130000
+      },
+      {
+        id: 3,
+        name: 'PRODUCT NAME 3',
+        image: '3.png',
+        price: 220000
+      },
+      {
+        id: 4,
+        name: 'PRODUCT NAME 4',
+        image: '4.PNG',
+        price: 125000
+      },
+      {
+        id: 5,
+        name: 'PRODUCT NAME 5',
+        image: '5.PNG',
+        price: 150000
+      },
+      {
+        id: 6,
+        name: 'PRODUCT NAME 6',
+        image: '6.PNG',
+        price: 160000
+      },
+
+];
